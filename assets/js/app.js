650808e9605b5c0809a4d22fa0cd6f7e56928a27
Yuriy <Lutiy2501.com@gmail.com>
2022-04-29 13:42:14 +0300

    bitwallet_demo

---
+++ assets/js/app.js
@@ -0,0 +1,207 @@
+// "use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    // accordion script
+
+    let collapsibleHeaders = document.querySelectorAll('.accordion__collapsible');
+
+    collapsibleHeaders.forEach(header => {
+        header.addEventListener('click', function () {
+            if (this.classList.contains('collapsible--open')) {
+                this.classList.remove('collapsible--open');
+            } else {
+                for (el of collapsibleHeaders) {
+                    el.classList.remove('collapsible--open');
+                }
+                this.classList.add('collapsible--open');
+            }
+        });
+    });
+
+    // accordion script
+
+
+    //catalog nav show
+    const catalogBtn = document.querySelector('#catalog__btn');
+    const catalogInner = document.querySelector('.catalog__inner');
+    catalogBtn.addEventListener('click', () => {
+        catalogInner.classList.toggle('openCatalog')
+    });
+
+
+    // smooth scroll
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
+
+            const blockID = anchor.getAttribute('href').substr(1)
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
+
+    // burger - btn
+    const burgerBtn = document.querySelector('#burger__btn');
+    const burgerClose = document.querySelector('#close__nav');
+    const burgerCloseFoot = document.querySelector('#close__nav-foot');
+
+    const burgerOpenFoot = document.querySelector('#burger__btn-foot');
+
+    const mobileNav = document.querySelector('.header__nav');
+    const mobileNavFoot = document.querySelector('.footer__nav-resp');
+
+    burgerBtn.addEventListener('click', () => {
+        mobileNav.classList.add('show');
+        document.querySelector('body').style.overflow = 'hidden';
+        document.querySelector('header .logo').style.opacity = '0';
+        document.querySelector('.overlay').classList.add('overlay__visible');
+        burgerBtn.style.opacity = '0'
+    });
+
+    burgerOpenFoot.addEventListener('click', () => {
+        mobileNavFoot.style.right = '-20px';
+        document.querySelector('.overlay').classList.add('overlay__visible');
+    });
+    burgerCloseFoot.addEventListener('click', () => {
+        mobileNavFoot.style.right = '-100%';
+        document.querySelector('.overlay').classList.remove('overlay__visible');
+
+    });
+
+    burgerClose.addEventListener('click', () => {
+        mobileNav.classList.remove('show');
+        document.querySelector('body').style.overflow = 'auto';
+        document.querySelector('header .logo').style.opacity = '1';
+        burgerBtn.style.opacity = '1';
+        document.querySelector('.overlay').classList.remove('overlay__visible');
+
+
+    });
+
+    // Sliders-----------------------------------------------------------
+    new Swiper('.slider__inner', {
+        fadeEffect: {
+            crossFade: true,
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-prev',
+            prevEl: '.swiper-button-next'
+        },
+
+    });
+
+    if (window.screen.width < 768) {
+        document.querySelector('.footer').classList.add('footer-resp');
+    }
+
+    if (window.screen.width < 376) {
+        let newsTitle = document.querySelector('#newsBannerTitle');
+        newsTitle.innerHTML = 'Лучшие Крипто- кошельки';
+
+    }
+
+    if (window.screen.width < 992) {
+        document.querySelector('.brands__inner').classList.remove('swiper-container');
+        document.querySelector('.brands__inner .row').classList.remove('swiper-wrapper');
+        const slideArr = document.querySelectorAll('.brands__inner .row div.swiper-slide');
+        slideArr.forEach(element => {
+            element.classList.remove('swiper-slide');
+        });
+    }
+
+
+
+    if (window.screen.width > 992) {
+
+        new Swiper('.brands__inner', {
+            slidesPerView: 6,
+            breakpoints: {
+
+                360: {
+                    slidesPerView: 'auto',
+                },
+
+            },
+
+            navigation: {
+                nextEl: '.swiper-button-prev',
+                prevEl: '.swiper-button-next'
+            },
+        });
+    }
+
+
+
+    new Swiper('.reviews__inner', {
+
+        breakpoints: {
+
+            992: {
+                slidesPerView: 3,
+
+            },
+
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-prev',
+            prevEl: '.swiper-button-next'
+        },
+    });
+
+    const tabs = document.querySelector('.tabs');
+    const tabsBtn = document.querySelectorAll('.tabs__btn');
+    const tabsContent = document.querySelectorAll('.tabs__content');
+
+    if (tabs) {
+        tabs.addEventListener('click', (e) => {
+            if (e.target.classList.contains('tabs__btn')) {
+                const tabsPath = e.target.dataset.tabsPath;
+                tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') });
+                document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs__btn--active');
+                tabsHandler(tabsPath);
+            }
+
+            if (e.target.classList.contains('tabs__arrow--prev')) {
+                let activeBtn = document.querySelector('.tabs__btn--active');
+                let activeParent = activeBtn.closest('.tabs__item');
+                let previousParent = activeParent.previousElementSibling;
+
+                if (previousParent) {
+                    let prevActive = previousParent.querySelector('.tabs__btn')
+                    tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') });
+                    prevActive.classList.add('tabs__btn--active');
+
+                    let path = prevActive.dataset.tabsPath;
+                    tabsHandler(path);
+                }
+            }
+
+            if (e.target.classList.contains('tabs__arrow--next')) {
+                let activeBtn = document.querySelector('.tabs__btn--active');
+                let activeParent = activeBtn.closest('.tabs__item');
+                let nextParent = activeParent.nextElementSibling;
+
+                if (nextParent) {
+                    let nextActive = nextParent.querySelector('.tabs__btn');
+                    tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') });
+                    nextActive.classList.add('tabs__btn--active');
+
+                    let path = nextActive.dataset.tabsPath;
+                    tabsHandler(path);
+                }
+            }
+        });
+    }
+
+    const tabsHandler = (path) => {
+        tabsContent.forEach(el => { el.classList.remove('tabs__content--active') });
+        document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content--active');
+    };
+});
